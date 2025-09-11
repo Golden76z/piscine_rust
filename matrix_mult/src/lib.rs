8c170497d4e5d1a800e31e50db0046cbc20419d9
Golden <damienprouet76@gmail.com>
2025-09-11 17:13:59 +0200

    Matrix mult

---
+++ matrix_mult/src/lib.rs
@@ -0,0 +1,54 @@
+use std::fmt::Debug;
+use std::ops::{Add, Mul};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T: Copy> Matrix<T> {
+    pub fn number_of_cols(&self) -> usize {
+        if self.0.is_empty() {
+            0
+        } else {
+            self.0[0].len()
+        }
+    }
+
+    pub fn number_of_rows(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn row(&self, n: usize) -> Vec<T> {
+        self.0[n].clone()
+    }
+
+    pub fn col(&self, n: usize) -> Vec<T> {
+        self.0.iter().map(|row| row[n]).collect()
+    }
+}
+
+impl<T: Mul + Copy + Default + Add<<T as Mul>::Output, Output = T>> Mul for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let rows = self.number_of_rows();
+        let cols = rhs.number_of_cols();
+
+        if self.number_of_cols() != rhs.number_of_rows() {
+            return None;
+        }
+
+        let mut result = vec![vec![T::default(); cols]; rows];
+
+        for i in 0..rows {
+            for j in 0..cols {
+                let mut sum = T::default();
+                for k in 0..self.number_of_cols() {
+                    sum = sum + self.0[i][k] * rhs.0[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+
+        Some(Matrix(result))
+    }
+}
